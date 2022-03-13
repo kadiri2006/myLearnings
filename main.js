106c8a51889ad66bb4c008fe0475e955e3261e46
kadiri2006 <kadiri2006@gmail.com>
2022-03-13 20:08:34 +0530

    at what cases promise pending we can observe

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 function addition(a, b) {
-  return new Promise((x, y) => {
-    x(a + b);
+  return new Promise((x, y) => {            //here we did not provide setTimeout
+    x(a + b);                                 
+    console.log("test:addition");
   });
 }
 
-addition(2, 3)
+function multiplication(a, b) {
+  return new Promise((x, y) => {
+    setTimeout(() => {                        //here we providing setTimeout
+      x(a * b);
+    }, 2000);
+    console.log("test:multiplication");
+  });
+}
+
+/* addition(2, 3)
   .then((x) => {
     console.log(x);
     return x + 10;
@@ -14,3 +24,7 @@ addition(2, 3)
     return x + 10;
   })
   .then((x) => console.log(x));
+ */
+
+console.log(addition(1, 2));      //Promise {<fulfilled>: 3}
+console.log(multiplication(2, 3));  //Promise {<pending>}
