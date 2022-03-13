4a00576b507062a52fc832af715b5904278717d4
kadiri2006 <kadiri2006@gmail.com>
2022-03-13 15:03:28 +0530

    with finally

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ try {
 } catch (error) {
   throw "throw error";
   console.log(error);
+} finally {
+  console.log("final line"); //this line is executed even presence  of "throw" in catch block because of "finally"
 }
-
-console.log("final line");   //this line not executed because of "throw" in catch block
