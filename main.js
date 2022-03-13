8cb19e7656e4316e09104c7c1d1abf28dc7d405b
kadiri2006 <kadiri2006@gmail.com>
2022-03-13 15:13:27 +0530

    handling throw error

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 try {
-  moveBy();
-} catch (error) {
+  // moveBy();
   throw "throw error";
+} catch (error) {
   console.log(error);
 } finally {
   console.log("final line"); //this line is executed even presence  of "throw" in catch block because of "finally"
 }
+
+//here we would get uncaught error because we did not handle the error("throw") in catch block
+
+//if we move that [throw "throw error"] from catch block to try
+//try block we would't get that uncaught error because we handleing
+//error produced by  try block in catch block .
