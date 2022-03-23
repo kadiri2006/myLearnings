a1e3fdc3c07abd93ac89493383b1de473ce930d5
kadiri2006 <kadiri2006@gmail.com>
2022-03-23 20:10:36 +0530

    next

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ function one() {
 
 
 
-one().then((x)=>console.log(x)).then((x)=>console.log(x)).then(()=>console.log("done2"))
+one().then((x)=>{console.log(x);return "from first"}).then((x)=>console.log(x)).then(()=>console.log("done2"))
