a91b66dc853240fd28d034e7f32dd71497fa1bc1
kadiri2006 <kadiri2006@gmail.com>
2022-04-16 13:39:11 +0530

    destructuring argumnets

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-let result = new Promise((res, rej) => {
-  res("sucess");
-  rej("failureeees");
-});
-
-async function test() {
-  try {
-    console.log(await result);
-  } catch (error) {
-    console.log(error);
-  }
-
-  
+function test() {
+  console.log("this value", this);
+  console.log("arguments value", arguments);
 }
 
+let testobj = {
+  name: "testing object",
+  test: function () {
+    console.log("this value ", this);
+    let x1 = { ...arguments };
+    let { ...x2 } = arguments;
+    console.log(x1);
+    console.log(x2);
+    let x3 = [...arguments];
+    let [...x4] = arguments;
+    console.log(x3);
+    console.log(x4);
+  },
+};
+
 test();
+testobj.test(1, 2, 3);
