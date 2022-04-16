66189cb23ab7d84fc9f703fe983ea59bedb50a16
kadiri2006 <kadiri2006@gmail.com>
2022-04-16 20:27:31 +0530

    settimeout

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-let a = 4;
-
-function test1(a) {
-   a = 5;
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => {
+    console.log(i);   //0,1,2,3,4 because i refers not value but storge and let is block scope but var is functional scope
+  }, i*1000);
 }
 
-test1(a);
-
-console.log(a);//4
-
-
-function test2() {
-  a = 5;
+for (var i = 0; i < 5; i++) {
+  setTimeout(() => {
+    console.log(i); //5,5,5,5,5
+  }, i * 1000);
 }
-
-test2();
-console.log(a);//5
-
