0685e67bb4b78278b71bfc8074269052337fc4bc
kadiri2006 <kadiri2006@gmail.com>
2022-05-03 11:03:09 +0530

    with providing arguments

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function outer() {
-  var a = "hello";
+var a = "hello";
+function outer(a) {
   return function inner() {
     console.log(a); //hello
   };
 }
 
-let res = outer(); // this "inner" fun  form closure.
+let res = outer(a);
 
 a = "hello to hi";
 res();
