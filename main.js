1c79eda0063bf664b2a508169117cd2962ae5793
kadiri2006 <kadiri2006@gmail.com>
2022-05-04 12:09:47 +0530

    shallow copy with JSON.stringify

--- main.js
+++ main.js
@@ -1,31 +1,63 @@
-//primitive values
-//string,boolean,null,undefind,number
+// 1
 
-let a = "hello";
-let b = a;
+/* let obj = {
+  name: "hello",
+  add: {
+    home: "home add",
+    office: {
+      office1: "office 1 add",
+      office2: "office 2 add",
+      meth: () => "office method",
+    },
+  },
+};
 
-b = "hello to hai";
+let obj2 = obj;
+obj2.add.office.office2 = "office 2 changed";
+console.log(obj2); //office 2 changed
+console.log(obj); //office 2 changed */
 
-console.log(a); //hello
-console.log(b); //hello to hai
+// 2
 
-//non-primitive values
-// arrays,objects,functions
+/* let obj = {
+  name: "hello",
+  add: {
+    home: "home add",
+    office: {
+      office1: "office 1 add",
+      office2: "office 2 add",
+      meth: () => "office method",
+    },
+  },
+};
 
-let nonA = { name: "hello" };
-let nonB = nonA;
+let obj2 = JSON.parse(JSON.stringify(obj));
+obj2.add.office.office2 = "office 2 changed";
 
-nonB.name = "hello to something";
+console.log(obj2); //office 2 changed
 
-console.log(nonA); //{name:"hello to something"}
-console.log(nonB); //{name:"hello to something"}
+// {name: 'hello', add: {…}}
+// add:
+// home: "home add"
+// office:
+// office1: "office 1 add"
+// office2: "office 2 changed"
+// [[Prototype]]: Object
+// [[Prototype]]: Object
+// name: "hello"
+// [[Prototype]]: Object
 
-//if you change by assign new obj
+console.log(obj); //office 2 add
 
-let objA = { name: "hello" };
-let objB = objA;
+// {name: 'hello', add: {…}}
+// add:
+// home: "home add"
+// office:
+// meth: () => "office method"
+// office1: "office 1 add"
+// office2: "office 2 add"
+// [[Prototype]]: Object
+// [[Prototype]]: Object
+// name: "hello"
+// [[Prototype]]: Object */
 
-objB = { name: "original does't changed" };
-
-console.log(objA); //{name: 'hello'}
-console.log(objB); //{name: "original does't changed"}
