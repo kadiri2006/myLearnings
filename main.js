8c15399d01d15fe4ac01d22235a35c42fc1c76c9
kadiri2006 <kadiri2006@gmail.com>
2022-05-04 16:07:48 +0530

    Object.assign

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 1
 
-/* let obj = {
+let obj = {
   name: "hello",
   add: {
     home: "home add",
@@ -12,52 +12,8 @@
   },
 };
 
-let obj2 = obj;
+let obj2 = Object.assign({}, obj);//it does not give deep clone
 obj2.add.office.office2 = "office 2 changed";
-console.log(obj2); //office 2 changed
-console.log(obj); //office 2 changed */
+console.log(obj2);//office 2 changed 
 
-// 2
-
-/* let obj = {
-  name: "hello",
-  add: {
-    home: "home add",
-    office: {
-      office1: "office 1 add",
-      office2: "office 2 add",
-      meth: () => "office method",
-    },
-  },
-};
-
-let obj2 = JSON.parse(JSON.stringify(obj));
-obj2.add.office.office2 = "office 2 changed";
-
-console.log(obj2); //office 2 changed
-
-// {name: 'hello', add: {…}}
-// add:
-// home: "home add"
-// office:
-// office1: "office 1 add"
-// office2: "office 2 changed"
-// [[Prototype]]: Object
-// [[Prototype]]: Object
-// name: "hello"
-// [[Prototype]]: Object
-
-console.log(obj); //office 2 add
-
-// {name: 'hello', add: {…}}
-// add:
-// home: "home add"
-// office:
-// meth: () => "office method"
-// office1: "office 1 add"
-// office2: "office 2 add"
-// [[Prototype]]: Object
-// [[Prototype]]: Object
-// name: "hello"
-// [[Prototype]]: Object */
-
+console.log(obj);//office 2 changed
