0a39c778f11bfdf7c9638a520b1c67550288e161
kadiri2006 <kadiri2006@gmail.com>
2022-05-04 10:14:47 +0530

    setTimeout at outof scope of var a

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function outer() {
   var a = 10;
-  setTimeout(() => {
-    a = 20;
-  }, 1000);
+
   return function inner() {
-    console.log(a); //20
+    console.log(a); //10
   };
 }
 
 let x = outer();
+
+setTimeout(() => {
+  a = 20; // this setTime out does't change "a" value in "outer" fun because of this setTimeout not in "var a=10" scope
+}, 1000);
 
 setTimeout(() => x(), 1000);
